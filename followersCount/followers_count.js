4bedc01f401418e564924ebd5c4eb4d44320af7b
paudelpawan <paudelpawan17@gmail.com>
2024-05-20 17:01:49 -0400

    Followers count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,17 @@
+let count=0;
+function increaseCount(){
+    count++;
+    displayCount();
+    checkCountValue();
+}
+function displayCount(){
+    document.getElementById("countDisplay").innerHTML= count;
+}
+function checkCountValue(){
+    if (count==10){
+        alert("Your account gained 10 followers. Congrats")
+    }
+    else if(count==20){
+        alert("Your account gained 20 followers. Keep it up! ")
+    }
+}
